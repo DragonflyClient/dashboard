4074f0b44d63e452020d644e74cc9c74c1c4e72c
flaaaps <julianwening4@gmail.com>
2020-09-27 14:54:12 +0200

    ✨ Add login page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express!' });
 });
 
+router.get('/login', (req, res) => {
+  res.render("login")
+})
+
 module.exports = router;
