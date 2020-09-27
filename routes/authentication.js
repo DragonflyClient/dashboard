9388ccf03f786df07780345c08131b587836b944
flaaaps <julianwening4@gmail.com>
2020-09-27 14:59:38 +0200

    🎨 Format code

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const axios = require('axios').default
+// const axios = require('axios').default
 
 router.post('/logout', async (req, res) => {
-    res.cookie('dragonfly-token', "expired", { maxAge: 1, httpOnly: true, domain: ".playdragonfly.net" });
-    res.send({ success: true, message: "Cleared cookie" })
+    res.cookie('dragonfly-token', "expired", { maxAge: 0, httpOnly: true, domain: ".playdragonfly.net" }).send({ success: true, message: "Cleared cookie" })
 })
 
 module.exports = router
