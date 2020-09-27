2469497c68e99e41f472d2b8a417d328963efc42
flaaaps <julianwening4@gmail.com>
2020-09-27 20:06:33 +0200

    🚀 Add "path" field

--- routes/authentication.js
+++ routes/authentication.js
@@ -3,7 +3,7 @@ const router = express.Router()
 // const axios = require('axios').default
 
 router.post('/logout', async (req, res) => {
-    res.cookie('dragonfly-token', "expired", { maxAge: 0, httpOnly: true, domain: ".playdragonfly.net" }).send({ success: true, message: "Cleared cookie" })
+    res.cookie('dragonfly-token', "expired", { maxAge: 0, httpOnly: true, domain: ".playdragonfly.net" }).send({ success: true, message: "Cleared cookie", path: req.path })
 })
 
 module.exports = router
