11d98746a20c444e78f16bcc6164c697e315b71a
flaaaps <julianwening4@gmail.com>
2020-10-03 14:23:15 +0200

    ✨ List correct data in playtime overview graph

--- public/js/charts/chart-area-demo.js
+++ public/js/charts/chart-area-demo.js
@@ -28,127 +28,136 @@ function number_format(number, decimals, dec_point, thousands_sep) {
   return s.join(dec);
 }
 
-const playTimeYear = [1];
+let myLineChart;
+let playTimeYear;
 
-fetch("https://dashboard.playdragonfly.net/statistics/ot/total")
-  .then(res => res.json())
-  .then(data => {
-    console.log(data)
-  })
+async function loadData() {
+  const result = await fetch("https://dashboard.playdragonfly.net/statistics/ot/month/all")
+  const json = await result.json()
+  playTimeYear = json.playTimeYear
 
-
-// Area Chart Example
-const ctx = document.getElementById("myAreaChart");
-const myLineChart = new Chart(ctx, {
-  type: "line",
-  data: {
-    labels: getPreviousMonths(12),
-    datasets: [
-      {
-        label: "Playtime",
-        lineTension: 0.1,
-        backgroundColor: "rgba(78, 115, 223, 0.05)",
-        borderColor: "rgba(78, 115, 223, 1)",
-        pointRadius: 3,
-        pointBackgroundColor: "rgba(78, 115, 223, 1)",
-        pointBorderColor: "rgba(78, 115, 223, 1)",
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
-        pointHoverBorderColor: "rgba(78, 115, 223, 1)",
-        pointHitRadius: 15,
-        pointBorderWidth: 1,
-        data: playTimeYear,
-      },
-    ],
-  },
-  options: {
-
-    animation: {
-      tension: {
-        easing: 'easeInOutCubic',
-        duration: 500 // general animation time
-      }
-    },
-    hover: {
-      animationDuration: 200 // duration of animations when hovering an item
-    },
-    responsiveAnimationDuration: 00, // animation duration after a resize
-    maintainAspectRatio: false,
-    layout: {
-      padding: {
-        left: 10,
-        right: 25,
-        top: 25,
-        bottom: 10,
-      },
-    },
-    scales: {
-      xAxes: [
+  // Area Chart Example
+  const ctx = document.getElementById("myAreaChart");
+  myLineChart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: getPreviousMonths(12),
+      datasets: [
         {
-          type: 'time',
-          time: {
-            unit: 'month',
-            tooltipFormat: 'MMM YYYY'
-          },
-          gridLines: {
-            display: false,
-            drawBorder: false,
-          },
-          ticks: {
-            maxTicksLimit: 12,
-            beginAtZero: false,
-          },
+          label: "Playtime",
+          lineTension: 0.1,
+          backgroundColor: "rgba(78, 115, 223, 0.05)",
+          borderColor: "rgba(78, 115, 223, 1)",
+          pointRadius: 3,
+          pointBackgroundColor: "rgba(78, 115, 223, 1)",
+          pointBorderColor: "rgba(78, 115, 223, 1)",
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
+          pointHoverBorderColor: "rgba(78, 115, 223, 1)",
+          pointHitRadius: 15,
+          pointBorderWidth: 1,
+          data: playTimeYear,
         },
       ],
-      yAxes: [
-        {
-          ticks: {
-            maxTicksLimit: 10,
-            padding: 10,
-            // Include a dollar sign in the ticks
-            callback: function (value, index, values) {
-              return value + "h";
+    },
+    options: {
+
+      animation: {
+        tension: {
+          easing: 'easeInOutCubic',
+          duration: 500 // general animation time
+        }
+      },
+      hover: {
+        animationDuration: 200 // duration of animations when hovering an item
+      },
+      responsiveAnimationDuration: 00, // animation duration after a resize
+      maintainAspectRatio: false,
+      layout: {
+        padding: {
+          left: 10,
+          right: 25,
+          top: 25,
+          bottom: 10,
+        },
+      },
+      scales: {
+        xAxes: [
+          {
+            type: 'time',
+            time: {
+              unit: 'month',
+              tooltipFormat: 'MMM YYYY'
+            },
+            gridLines: {
+              display: false,
+              drawBorder: false,
+            },
+            ticks: {
+              maxTicksLimit: 12,
+              beginAtZero: false,
             },
           },
-          gridLines: {
-            circular: true,
-            color: "rgb(234, 236, 244)",
-            zeroLineColor: "rgb(234, 236, 244)",
-            drawBorder: false,
-            borderDash: [2],
-            drawTicks: true,
-            zeroLineBorderDash: [2],
+        ],
+        yAxes: [
+          {
+            ticks: {
+              maxTicksLimit: 10,
+              padding: 10,
+              // Include a dollar sign in the ticks
+              callback: function (value, index, values) {
+                return formatHours(value)
+              },
+            },
+            gridLines: {
+              circular: true,
+              color: "rgb(234, 236, 244)",
+              zeroLineColor: "rgb(234, 236, 244)",
+              drawBorder: false,
+              borderDash: [2],
+              drawTicks: true,
+              zeroLineBorderDash: [2],
+            },
           },
-        },
-      ],
-    },
-    legend: {
-      display: false,
-    },
-    tooltips: {
-      backgroundColor: "rgb(255,255,255)",
-      bodyFontColor: "#858796",
-      titleMarginBottom: 10,
-      titleFontColor: "#6E707E",
-      titleFontSize: 14,
-      borderColor: "#DDDFEB",
-      borderWidth: 1,
-      xPadding: 15,
-      yPadding: 15,
-      displayColors: false,
-      intersect: false,
-      mode: "index",
-      caretPadding: 10,
-      callbacks: {
-        label: function (tooltipItem, chart) {
-          const datasetLabel =
-            chart.datasets[tooltipItem.datasetIndex].label || "";
-          return datasetLabel + ": " + tooltipItem.yLabel + "h";
+        ],
+      },
+      legend: {
+        display: false,
+      },
+      tooltips: {
+        backgroundColor: "rgb(255,255,255)",
+        bodyFontColor: "#858796",
+        titleMarginBottom: 10,
+        titleFontColor: "#6E707E",
+        titleFontSize: 14,
+        borderColor: "#DDDFEB",
+        borderWidth: 1,
+        xPadding: 15,
+        yPadding: 15,
+        displayColors: false,
+        intersect: false,
+        mode: "index",
+        caretPadding: 10,
+        callbacks: {
+          label: function (tooltipItem, chart) {
+            const datasetLabel =
+              chart.datasets[tooltipItem.datasetIndex].label || "";
+            return datasetLabel + ": " + formatHours(tooltipItem.yLabel);
+          },
         },
       },
     },
-  },
-});
+  });
+}
+
+loadData()
+
+function formatHours(value) {
+  const raw = value * 60
+  const hours = Math.trunc(raw / 60);
+  const minutes = raw % 60
+  return hours > 0 && minutes > 0 ? `${hours}h ${minutes}m` : hours > 0 ? `${hours}h` : `${minutes}m`;
+}
 
 function getPreviousMonths(length) {
   var months = [];
@@ -167,8 +176,6 @@ function addData(chart, length, current) {
   } else {
     chart.options.animation.duration = 500
     const monthly = playTimeYear.slice(Math.max(playTimeYear.length - 7, 1))
-    console.log(chart.data.datasets[0].data)
-    console.log(monthly)
     chart.data.datasets[0].data = monthly
   }
   chart.data.labels = 0;
