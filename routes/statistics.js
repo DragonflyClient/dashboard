64562387b49ef4dae7c06eed5ec38deb148ec57d
flaaaps <julianwening4@gmail.com>
2020-10-03 10:20:43 +0200

    ✨ Add route for full monthly playtime

--- routes/statistics.js
+++ routes/statistics.js
@@ -12,21 +12,33 @@ router.get('/ot/total', async (req, res, next) => {
     const statistics = await mongoose.connection.db.collection('statistics').findOne({ dragonflyUUID: dragonflyUUID });
 
     let totalPlaytime = 0
-    let monthlyPlaytime = 0
 
     // account.creationDate = moment(account.creationDate).format('LL');
 
     if (statistics) {
         totalPlaytime = statistics.onlineTime.total
+    }
+
+    res.send({ success: true, totalPlaytime: totalPlaytime })
+})
+
+router.get('/ot/month/all', async (req, res) => {
+    const token = req.cookies['dragonfly-token']
+    const account = await getDragonflyAccount(token)
+    const dragonflyUUID = account.uuid
+    const statistics = await mongoose.connection.db.collection('statistics').findOne({ dragonflyUUID: dragonflyUUID });
+
+    const fullPlaytimes = []
+    if (statistics) {
+        totalPlaytime = statistics.onlineTime.total
         for (var key in statistics.onlineTime) {
-            if (statistics.onlineTime.hasOwnProperty(key)) {
-                const contains = key.split('/')[0] == new Date().getMonth() + 1
-                if (contains) monthlyPlaytime = statistics.onlineTime[key]
+            if (statistics.onlineTime.hasOwnProperty(key) && key !== "total") {
+                fullPlaytimes.push(key + ":" + statistics.onlineTime[key])
             }
         }
     }
-
-    res.send({ totalPlaytime: totalPlaytime, monthlyPlaytime: monthlyPlaytime })
+    console.log(fullPlaytimes)
+    res.send({ success: true, totalMonthPlaytime: fullPlaytimes })
 })
 
 async function getDragonflyAccount(token) {
