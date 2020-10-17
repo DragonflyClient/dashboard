0f43d51c9d2890b277d4bc392d85a252e998e566
flaaaps <julianwening4@gmail.com>
2020-10-17 17:06:47 +0200

    🐛 Handle username/email change response

--- public/js/account.js
+++ public/js/account.js
@@ -1,43 +1,54 @@
-$(function () {
-    function adjust(elements, offset, min, max) {
-        // initialize parameters
-        offset = offset || 0;
-        min = min || 0;
-        max = max || Infinity;
-        elements.each(function () {
-            var element = $(this);
-            // add element to measure pixel length of text
-            var id = btoa(Math.floor(Math.random() * Math.pow(2, 64)));
-            var tag = $('<span id="' + id + '">' + element.val() + '</span>').css({
-                'display': 'none',
-                'font-family': element.css('font-family'),
-                'font-size': element.css('font-size'),
-            }).appendTo('body');
-            // adjust element width on keydown
-            function update() {
-                // give browser time to add current letter
-                setTimeout(function () {
-                    // prevent whitespace from being collapsed
-                    tag.html(element.val().replace(/ /g, '&nbsp'));
-                    // clamp length and prevent text from scrolling
-                    var size = Math.max(min, Math.min(max, tag.width() + offset));
-                    if (size < max)
-                        element.scrollLeft(0);
-                    // apply width to element
-                    element.width(size);
-                }, 0);
-            };
-            update();
-            element.keydown(update);
-        });
-    }
-    // apply to our element
-    adjust($('.adjust'), 10, 20, 500);
-});
+function adjust(elements, offset, min, max) {
+    // initialize parameters
+    offset = offset || 0;
+    min = min || 0;
+    max = max || Infinity;
+    elements.each(function () {
+        var element = $(this);
+        // add element to measure pixel length of text
+        var id = btoa(Math.floor(Math.random() * Math.pow(2, 64)));
+        var tag = $('<span id="' + id + '">' + element.val() + '</span>').css({
+            'display': 'none',
+            'font-family': element.css('font-family'),
+            'font-size': element.css('font-size'),
+        }).appendTo('body');
+        // adjust element width on keydown
+        function update() {
+            // give browser time to add current letter
+            setTimeout(function () {
+                // prevent whitespace from being collapsed
+                tag.html(element.val().replace(/ /g, '&nbsp'));
+                // clamp length and prevent text from scrolling
+                var size = Math.max(min, Math.min(max, tag.width() + offset));
+                if (size < max)
+                    element.scrollLeft(0);
+                // apply width to element
+                element.width(size);
+            }, 0);
+        };
+        update();
+        element.keydown(update);
+    });
+}
+// apply to our element
+adjust($('.adjust'), 10, 20, 500);
 
 const accountDataInput = document.querySelectorAll('.account__input')
+const accountDataIcon = document.querySelectorAll('.account__input-icon')
 
-console.log(accountDataInput)
+accountDataIcon.forEach(icon => {
+    icon.addEventListener('click', (e) => {
+        console.log(e.target.previousElementSibling)
+        const target = e.target.previousElementSibling
+        console.log(target)
+        if (target.dataset.inputType === "email") {
+            setEmail(target.dataset.default, target, 0000)
+        } else if (target.dataset.inputType === "username") {
+            setUsername(target.dataset.default, target, 0000)
+        }
+    })
+})
+
 accountDataInput.forEach(input => {
     input.addEventListener('keyup', (e) => {
         if (e.target.dataset.inputType === "email") {
@@ -48,13 +59,14 @@ accountDataInput.forEach(input => {
     })
 })
 
+
+
 async function setEmail(email, input, key) {
-    console.log(input.value)
     if (!validateInput(email, input.value)) {
         input.nextElementSibling.innerHTML = '<svg class="account__username-icon" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="pen" class="svg-inline--fa fa-pen fa-w-16" role="img" viewBox="0 0 512 512"><path fill="currentColor" d="M290.74 93.24l128.02 128.02-277.99 277.99-114.14 12.6C11.35 513.54-1.56 500.62.14 485.34l12.7-114.22 277.9-277.88zm207.2-19.06l-60.11-60.11c-18.75-18.75-49.16-18.75-67.91 0l-56.55 56.55 128.02 128.02 56.55-56.55c18.75-18.76 18.75-49.16 0-67.91z"/></svg>'
     } else {
         input.nextElementSibling.innerHTML = '<svg class="account__username-icon" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" class="svg-inline--fa fa-check fa-w-16" role="img" viewBox="0 0 512 512"><path fill="currentColor" d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"/></svg>'
-        if (key == '13') {
+        if (key == '13' || key == 0000) {
             const result = await fetch(`https://email-verification.playdragonfly.net/change`, {
                 method: 'POST',
                 credentials: "include",
@@ -64,7 +76,26 @@ async function setEmail(email, input, key) {
                 body: JSON.stringify({ email: input.value })
             })
             const data = await result.json()
-            console.log(data, "DAD")
+            console.log(data)
+            if (data.success) {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Nice one!',
+                    text: `Sent verification email to "${input.value}". Click the link in the email to update your email address.`
+                })
+            } else if (data.next) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Damn',
+                    text: `You can change your email address again ${moment(data.next).fromNow()}`,
+                })
+            } else {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Whoops.',
+                    text: data.message ? data.message : "Something went wrong please try again later.",
+                })
+            }
         }
     }
 }
@@ -78,7 +109,7 @@ async function setUsername(username, input, key) {
         input.nextElementSibling.innerHTML = '<svg class="account__username-icon" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="pen" class="svg-inline--fa fa-pen fa-w-16" role="img" viewBox="0 0 512 512"><path fill="currentColor" d="M290.74 93.24l128.02 128.02-277.99 277.99-114.14 12.6C11.35 513.54-1.56 500.62.14 485.34l12.7-114.22 277.9-277.88zm207.2-19.06l-60.11-60.11c-18.75-18.75-49.16-18.75-67.91 0l-56.55 56.55 128.02 128.02 56.55-56.55c18.75-18.76 18.75-49.16 0-67.91z"/></svg>'
     } else {
         input.nextElementSibling.innerHTML = '<svg class="account__username-icon" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" class="svg-inline--fa fa-check fa-w-16" role="img" viewBox="0 0 512 512"><path fill="currentColor" d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"/></svg>'
-        if (key == '13') {
+        if (key == '13' || key == 0000) {
             const result = await fetch('https://api.playdragonfly.net/v1/authentication/rename', {
                 method: "POST",
                 credentials: "include",
@@ -89,10 +120,12 @@ async function setUsername(username, input, key) {
             })
             const data = await result.json()
             if (data.success) {
+                console.log(input.value)
+                setNewDefault('username', input.value, input)
                 Swal.fire({
                     icon: 'success',
                     title: 'Nice one!',
-                    text: `Username changed to "${input.value}"`
+                    text: `Username changed to "${bodyData.name}"`
                 })
             } else if (data.next) {
                 Swal.fire({
@@ -100,6 +133,13 @@ async function setUsername(username, input, key) {
                     title: 'Damn',
                     text: `You can change your username again ${moment(data.next).fromNow()}`,
                 })
+                input.value = username
+                if (!validateInput(username, input.value)) {
+                    input.nextElementSibling.innerHTML = '<svg class="account__username-icon" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="pen" class="svg-inline--fa fa-pen fa-w-16" role="img" viewBox="0 0 512 512"><path fill="currentColor" d="M290.74 93.24l128.02 128.02-277.99 277.99-114.14 12.6C11.35 513.54-1.56 500.62.14 485.34l12.7-114.22 277.9-277.88zm207.2-19.06l-60.11-60.11c-18.75-18.75-49.16-18.75-67.91 0l-56.55 56.55 128.02 128.02 56.55-56.55c18.75-18.76 18.75-49.16 0-67.91z"/></svg>'
+                } else {
+                    input.nextElementSibling.innerHTML = '<svg class="account__username-icon" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" class="svg-inline--fa fa-check fa-w-16" role="img" viewBox="0 0 512 512"><path fill="currentColor" d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"/></svg>'
+                }
+                adjust($('.adjust'), 10, 20, 500);
             } else {
                 Swal.fire({
                     icon: 'error',
@@ -114,4 +154,45 @@ async function setUsername(username, input, key) {
 function validateInput(initial, current) {
     if (initial === current.trim()) return false
     else return true
+}
+
+function setNewDefault(type, value, el) {
+    console.log(type, value, el, "set")
+    if (type == "username") {
+        // defaultUsername = username
+        el.value = value
+        el.dataset.default = value
+    }
+    const username = el.dataset.default
+    console.log(username, value)
+    if (!validateInput(username, value)) {
+        el.nextElementSibling.innerHTML = '<svg class="account__username-icon" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="pen" class="svg-inline--fa fa-pen fa-w-16" role="img" viewBox="0 0 512 512"><path fill="currentColor" d="M290.74 93.24l128.02 128.02-277.99 277.99-114.14 12.6C11.35 513.54-1.56 500.62.14 485.34l12.7-114.22 277.9-277.88zm207.2-19.06l-60.11-60.11c-18.75-18.75-49.16-18.75-67.91 0l-56.55 56.55 128.02 128.02 56.55-56.55c18.75-18.76 18.75-49.16 0-67.91z"/></svg>'
+    } else {
+        el.nextElementSibling.innerHTML = '<svg class="account__username-icon" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" class="svg-inline--fa fa-check fa-w-16" role="img" viewBox="0 0 512 512"><path fill="currentColor" d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"/></svg>'
+    }
+}
+
+const uuidInput = document.getElementById('uuid-input')
+
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'bottom-start',
+    showConfirmButton: false,
+    timer: 2000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+})
+
+function copyUUID() {
+    uuidInput.select();
+    uuidInput.setSelectionRange(0, 99999)
+    document.execCommand("copy");
+
+    Toast.fire({
+        icon: 'success',
+        title: 'UUID copied to clipboard!'
+    })
 }
