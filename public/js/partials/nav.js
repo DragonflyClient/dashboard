76f6d9db4bd7202f9acde49607712e5399c1003d
flaaaps <julianwening4@gmail.com>
2020-10-27 16:14:33 +0100

    ✨ Mark all new notifications as read

--- public/js/partials/nav.js
+++ public/js/partials/nav.js
@@ -1,4 +1,14 @@
-console.log('nav')
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 1200,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+})
 const logoutBtn = document.getElementById('logout-btn')
 logoutBtn.addEventListener('click', async (e) => {
     const result = await fetch('/auth/logout', {
@@ -12,30 +22,9 @@ logoutBtn.addEventListener('click', async (e) => {
     if (data.success) window.location.href = `https://playdragonfly.net/login?ref=${window.location.href}`
 })
 
-let notifications = [{
-    icon: "information",
-    category: "Username",
-    message: `You've changed your username to "julii"`,
-    send_time: 1567548000, // Mittwoch, 4. September
-    action: null,
-    read: true,
-    read_time: null
-}, {
-    icon: "gift",
-    category: "Cosmetics",
-    message: `You've received **Dragonfly Wings** by **moritz**!`,
-    send_time: 1568152800, // Mittwoch, 11. September
-    action: {
-        type: "open_url!",
-        target:
-            "https://dashboard.playdragonfly.net/cosmetics?qualifier=ABCDE-FGHJI-KLMN-OPQRSTU"
-    },
-    read: false,
-    read_time: null
-}];
-
 const notificationCounter = document.getElementById('notification-counter')
 const notificationList = document.getElementById('notification-list')
+let notifications = []
 fetch('https://api.playdragonfly.net/v1/community/notifications', {
     credentials: 'include'
 }).then(res => res.json())
@@ -68,7 +57,6 @@ function listNotifications() {
     //           | Optional |
     let unread = 0
     notifications.reverse().forEach(notification => {
-        console.log(notification.message)
         if (!notification.read) {
             unread++
         }
@@ -76,18 +64,16 @@ function listNotifications() {
         Object.keys(types).map(function (key, index) {
             if (notification.icon == key) type = types[key];
         });
-        const sendTime = moment(notification.sendTime).format('LL')
+        const sendTime = moment(notification.sendTime).fromNow()
         var nth = 0;
-        console.log(notification.message, "MESSa");
         let notificationMessage = notification.message
         if (notificationMessage.indexOf('**') > -1)
             notificationMessage = notification.message.replace(/\*\*/g, function (match, i, original) {
                 nth++;
                 return nth % 2 === 0 ? "</b>" : "<b>";
             });
-        console.log(notification.action)
         notificationList.innerHTML += `
-    <a onclick='notificationAction("${notification.action && notification.action.type === "open_url" ? notification.action.target : ''}", "${notification._id}", ${notification.read})' class="${!notification.read ? type.border : ''} notification-item dropdown-item d-flex align-items-center cursor-pointer">
+    <a data-nid="${notification._id}" data-nread="${notification.read}" data-notification-read="${notification.read}" onclick='notificationAction("${notification.action && notification.action.type === "open_url" ? notification.action.target : ''}", "${notification._id}", ${notification.read})' class="${!notification.read ? type.border : ''} notification-item dropdown-item d-flex align-items-center cursor-pointer">
         <div class="mr-3">
             <div class="icon-circle ${type.bgColor}">
                 <i class="fas ${type.icon} text-white"></i>
@@ -99,14 +85,16 @@ function listNotifications() {
         </div>
     </a>`
     })
-    if (unread != 0)
-        notificationCounter.innerText = unread + "+"
-    else notificationCounter.innerText = ''
-    console.log(unread, "UNREAD")
+    if (unread <= 9 && unread > 0) {
+        notificationCounter.innerText = unread
+    } else if (unread > 9) {
+        notificationCounter.innerText = '9+'
+    } else {
+        notificationCounter.innerText = ''
+    }
 }
 
 async function notificationAction(actionURL, notificationId, read) {
-    console.log(read, !read)
     await fetch(`https://api.playdragonfly.net/v1/community/notifications/${notificationId}`, {
         credentials: 'include',
         method: 'PATCH'
@@ -116,7 +104,6 @@ async function notificationAction(actionURL, notificationId, read) {
             console.log(data, "READ")
         })
     // Set to read via API
-    console.log(actionURL)
     if (actionURL && actionURL.length > 0) {
         window.location.href = actionURL
     }
@@ -129,9 +116,56 @@ async function notificationAction(actionURL, notificationId, read) {
                 notifications = data.notifications
                 notificationList.innerHTML = ''
                 listNotifications()
+                console.log('notifications')
             })
             .catch(err => {
                 console.log(err)
             })
     }
+}
+
+const readAllBtn = document.getElementById('read-all-icon')
+
+readAllBtn.addEventListener('click', () => {
+    readAll()
+})
+
+async function readAll() {
+    let allRead = true
+    notificationList.childNodes.forEach(notification => {
+        if (!notification.tagName) return false
+        console.log(notification.dataset.nread, "ADDD")
+        if (notification.dataset.nread == "false") allRead = false
+    })
+    console.log(allRead)
+    if (!allRead) {
+        console.log('Setting all to read...')
+        await fetch(`https://api.playdragonfly.net/v1/community/notifications/all`, {
+            credentials: 'include',
+            method: 'PATCH'
+        })
+            .then(res => res.json())
+            .then(async data => {
+                if (data.success) {
+                    Toast.fire({
+                        icon: 'success',
+                        title: 'Set all notification to read'
+                    })
+                    console.log('Loading new...')
+                    await fetch('https://api.playdragonfly.net/v1/community/notifications', {
+                        credentials: 'include'
+                    }).then(res => res.json())
+                        .then(data => {
+                            notificationList.innerHTML = ''
+                            notifications = data.notifications
+                            listNotifications()
+                            console.log('notifications')
+                        })
+                        .catch(err => {
+                            console.log(err)
+                        })
+                }
+            })
+    }
+    console.log(allRead)
 }
