bddaf73a6d66448c07ad386bb834754f4d4ca0fc
flaaaps <julianwening4@gmail.com>
2020-10-30 18:42:14 +0100

    🐛 Make Chart.js work in Firefox

--- public/js/charts/chart-area-demo.js
+++ public/js/charts/chart-area-demo.js
@@ -30,67 +30,50 @@ function number_format(number, decimals, dec_point, thousands_sep) {
 
 const ctx = document.getElementById("myAreaChart");
 let playTimeYear = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-let myLineChart = new Chart(ctx, {
-  type: "line",
+var myLineChart = new Chart(ctx, {
+  type: 'line',
   data: {
     labels: getPreviousMonths(12),
-    datasets: [
-      {
-        label: "Playtime",
-        lineTension: 0.1,
-        backgroundColor: "rgba(78, 115, 223, 0.05)",
-        borderColor: "rgba(78, 115, 223, 1)",
-        pointRadius: 3,
-        pointBackgroundColor: "rgba(78, 115, 223, 1)",
-        pointBorderColor: "rgba(78, 115, 223, 1)",
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
-        pointHoverBorderColor: "rgba(78, 115, 223, 1)",
-        pointHitRadius: 15,
-        pointBorderWidth: 1,
-        data: playTimeYear,
-      },
-    ],
+    datasets: [{
+      label: "Playtime",
+      lineTension: 0.3,
+      backgroundColor: "rgba(78, 115, 223, 0.05)",
+      borderColor: "rgba(78, 115, 223, 1)",
+      pointRadius: 3,
+      pointBackgroundColor: "rgba(78, 115, 223, 1)",
+      pointBorderColor: "rgba(78, 115, 223, 1)",
+      pointHoverRadius: 3,
+      pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
+      pointHoverBorderColor: "rgba(78, 115, 223, 1)",
+      pointHitRadius: 10,
+      pointBorderWidth: 2,
+      data: playTimeYear,
+    }],
   },
   options: {
-
-    animation: {
-      tension: {
-        easing: 'easeInOutCubic',
-        duration: 500 // general animation time
-      }
-    },
-    hover: {
-      animationDuration: 200 // duration of animations when hovering an item
-    },
-    responsiveAnimationDuration: 00, // animation duration after a resize
     maintainAspectRatio: false,
     layout: {
       padding: {
         left: 10,
         right: 25,
         top: 25,
-        bottom: 10,
-      },
+        bottom: 0
+      }
     },
     scales: {
-      xAxes: [
-        {
-          type: 'time',
-          time: {
-            unit: 'month',
-            tooltipFormat: 'MMM YYYY'
-          },
-          gridLines: {
-            display: false,
-            drawBorder: false,
-          },
-          ticks: {
-            maxTicksLimit: 12,
-            beginAtZero: false,
-          },
+      xAxes: [{
+        time: {
+          unit: 'date',
+          tooltipFormat: 'MMM YYYY'
         },
-      ],
+        gridLines: {
+          display: false,
+          drawBorder: false
+        },
+        ticks: {
+          maxTicksLimit: 7
+        }
+      }],
       yAxes: [
         {
           ticks: {
@@ -114,21 +97,21 @@ let myLineChart = new Chart(ctx, {
       ],
     },
     legend: {
-      display: false,
+      display: false
     },
     tooltips: {
       backgroundColor: "rgb(255,255,255)",
       bodyFontColor: "#858796",
       titleMarginBottom: 10,
-      titleFontColor: "#6E707E",
+      titleFontColor: '#6e707e',
       titleFontSize: 14,
-      borderColor: "#DDDFEB",
+      borderColor: '#dddfeb',
       borderWidth: 1,
       xPadding: 15,
       yPadding: 15,
       displayColors: false,
       intersect: false,
-      mode: "index",
+      mode: 'index',
       caretPadding: 10,
       callbacks: {
         label: function (tooltipItem, chart) {
@@ -137,8 +120,8 @@ let myLineChart = new Chart(ctx, {
           return datasetLabel + ": " + formatHours(tooltipItem.yLabel);
         },
       },
-    },
-  },
+    }
+  }
 });
 window.addEventListener('load', () => {
   loadData()
