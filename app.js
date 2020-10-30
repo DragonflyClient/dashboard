e510a68a74c838ae7e24e4d47c2eae7a23a85726
flaaaps <julianwening4@gmail.com>
2020-10-30 19:07:05 +0100

    🐛 Add error page for unreachable routes

--- app.js
+++ app.js
@@ -54,6 +54,25 @@ app.use('/auth', authRouter);
 app.use('/minecraft', minecraftRouter)
 app.use('/statistics', statisticsRouter)
 
+app.use(function (req, res, next) {
+    res.status(404);
+
+    // respond with html page
+    if (req.accepts('html')) {
+        res.render('sites/404');
+        return;
+    }
+
+    // respond with json
+    if (req.accepts('json')) {
+        res.send({ error: 'Not found' });
+        return;
+    }
+
+    // default to plain-text. send()
+    res.type('txt').send('Not found');
+});
+
 async function getDragonflyAccount(token) {
     let account;
     await axios.post(`${BASE_API_URL}/v1/authentication/token`, {}, {
