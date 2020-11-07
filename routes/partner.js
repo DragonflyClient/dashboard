800f71586515331903dd6db9356b8e01989e5df2
flaaaps <julianwening4@gmail.com>
2020-11-07 19:49:28 +0100

    ✨ Display referral revenue

--- routes/partner.js
+++ routes/partner.js
@@ -25,7 +25,11 @@ router.get('/overview', async (req, res) => {
 
 	const dragonflyUUID = account.uuid;
 
-	let refAmount;
+	let totalAmount = '0€';
+	let totalCount = 0;
+
+	let registrationAmount = '0€';
+	let registrationCount = 0;
 
 	await axios
 		.get(`${BASE_API_URL}/v1/partner/referral`, {
@@ -34,13 +38,24 @@ router.get('/overview', async (req, res) => {
 			}
 		})
 		.then(result => {
-			console.log(result.data, 'RES');
+			console.log(result.data.referral, 'RES');
+			const referral = result.data.referral;
+
+			const insights = referral.insights;
+			const keys = Object.keys(insights);
+			const totalTimesReceived = keys.length == 0 ? 0 : keys.length == 1 ? insights[keys[0]].timesReceived : keys.reduce((accumulator, current) => insights[accumulator].timesReceived + insights[current].timesReceived);
+
+			totalAmount = (result.data.referral.creditInCents / 100).toFixed(2) + '€';
+			totalCount = totalTimesReceived;
+
+			registrationAmount = (insights.createAccounts.creditInCents / 100).toFixed(2) + '€';
+			registrationCount = insights.createAccounts.timesReceived;
 		})
 		.catch(err => {
 			console.log(err.response.data);
-			refAmount = err.response.data.error;
+			totalAmount = err.response.data.error;
 		});
-	console.log(refAmount, 'RA');
+	console.log(totalAmount, 'RA', totalCount);
 
 	// if (!refLink) return res.render('sites/partner/overview', { success: false, account: account, ref: refDetails || null, path: req.path, message: `Your account is not connected with a ref link.` });
 
@@ -51,13 +66,12 @@ router.get('/overview', async (req, res) => {
 	// refDetails.creationDate = refBonus.creationDate;
 	// refDetails.count = refBonus.count;
 
-	res.render('sites/partner/overview', { account: account, path: req.path, success: true, refAmount: refAmount });
+	res.render('sites/partner/overview', { account: account, path: req.path, success: true, totalAmount: totalAmount, totalReceived: totalCount, registrationCount: registrationCount, registrationAmount: registrationAmount });
 });
 
 // Api routes
 router.get('/ref/info', async (req, res) => {
 	const token = req.cookies['dragonfly-token'];
-	const account = req.account;
 
 	axios
 		.get(`${BASE_API_URL}/v1/partner/referral`, {
